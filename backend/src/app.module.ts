ce8d32c7e22b9df3724310358453f2b1856e4be7
hfidelis <heitorc88@gmail.com>
2025-10-14 12:34:11 -0300

    feat: register modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,8 +4,16 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CompaniesModule } from './companies/companies.module';
+import { EmployeesModule } from './employees/employees.module';
+import { AssetsModule } from './assets/assets.module';
 @Module({
-  imports: [AuthModule, UsersModule, CompaniesModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    CompaniesModule,
+    EmployeesModule,
+    AssetsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
