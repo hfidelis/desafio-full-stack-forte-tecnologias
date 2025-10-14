023a87c934ec041e78ab01e3e6c45c7473ed6a0a
hfidelis <heitorc88@gmail.com>
2025-10-14 02:38:09 -0300

    feat: register modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CompaniesModule } from './companies/companies.module';
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CompaniesModule],
   controllers: [AppController],
   providers: [AppService],
 })
